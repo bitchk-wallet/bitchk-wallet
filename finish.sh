e24eab6ab96ef1b91f2a71820a5fa2bb487c1255
user <>
2017-11-23 22:23:50 +0900

    r

--- finish.sh
+++ finish.sh
@@ -7,3 +7,4 @@ then
 fi
 
 npm run clean-all&&npm run apply:target --target=$1&&npm run final:android --pass=$2&&npm run final:ios;npm run open:ios;grunt pack-all
+#cordova run android --device;cordova run ios --device
