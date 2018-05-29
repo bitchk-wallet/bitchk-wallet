19582861b979d0b49ccac4144f0a502652c65a31
user <>
2018-05-29 17:48:32 +0900

    add script

---
+++ finish.and.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]
+then
+  echo "must 2 param first target second store pass"
+  echo "ex) finish.sh yangcoin yangchigi"
+  exit
+fi
+
+npm run clean-all&&git pull&&npm run apply:target2 --appname=$1&&npm run final:android --pass=$2&&grunt pack-all
+#cordova run android --device;cordova run ios --device
