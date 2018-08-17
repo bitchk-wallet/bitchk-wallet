d3cca92cb36b8bc32e7c038c16c9a3a8ae2c0bc3
user <>
2018-08-17 16:36:15 +0900

    change log info

--- src/js/services/logHeader.js
+++ src/js/services/logHeader.js
@@ -2,6 +2,6 @@
 angular.module('copayApp.services')
   .factory('logHeader', function($window, appConfigService, $log, platformInfo) {
     $log.info(appConfigService.nameCase + ' v' + $window.version + ' #' + $window.commitHash);
-    $log.info('Client: ' + JSON.stringify(platformInfo));
+    
     return {};
   });
